d67639d7a64ecefb5727375b5e560e7a58fd4385
Sandymuthu <109937883+Sandymuthu@users.noreply.github.com>
2022-10-29 16:22:17 +0530

    change b

--- First.java
+++ First.java
@@ -3,7 +3,7 @@ public class First
     
     public static void main(String[] args) {
         int a=23;
-      int b=34;
+      int b=100;
         System.out.println("Program is running");
 
         System.out.println(a+b);
