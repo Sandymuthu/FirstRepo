ee7a02c420fb1db1670772a1872f4fd4c8e9ff9a
unknown <sandhiya.sep4@gmail.com>
2022-10-29 16:20:26 +0530

    Second change

--- First.java
+++ First.java
@@ -1,6 +1,11 @@
 public class First
 {
+    
     public static void main(String[] args) {
+        int a=23;
+      int b=34;
         System.out.println("Program is running");
+
+        System.out.println(a+b);
     }
 }
